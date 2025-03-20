1c1d86947cd866b73fa9f7fbcf23329bcdc6c3a6
pmillotpro <pierre.millot.pro@gmail.com>
2025-03-20 08:46:40 +0100

    Créer les activités

---
+++ plug-in-pool-mobile/app/src/main/java/com/example/plug_in_pool/configurationMatch.java
@@ -0,0 +1,16 @@
+package com.example.plug_in_pool;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class configurationMatch extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_configuration_match);
+    }
+}
