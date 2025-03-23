5940b26e550d1768595cb33e0dbdf7edf654f9e1
pmillotpro <pierre.millot.pro@gmail.com>
2025-03-23 12:40:05 +0100

    Ajouter les classes

--- plug-in-pool-mobile/app/src/main/java/com/example/plug_in_pool/configurationMatch.java
+++ plug-in-pool-mobile/app/src/main/java/com/example/plug_in_pool/configurationMatch.java
@@ -4,13 +4,24 @@ import android.os.Bundle;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
+import java.time.LocalDateTime;
 
-public class configurationMatch extends AppCompatActivity {
-
+public class configurationMatch extends AppCompatActivity
+{
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_configuration_match);
+    }
+    public class Manche
+    {
+        int numeroTable;
+        LocalDateTime horodatage;
+        int gagnant;
+        int joueurTable;
+        boolean couleursDefinies;
+        int duree;
     }
 }
--- plug-in-pool-mobile/app/src/main/java/com/example/plug_in_pool/gestionMatch.java
+++ plug-in-pool-mobile/app/src/main/java/com/example/plug_in_pool/gestionMatch.java
@@ -1,16 +1,37 @@
 package com.example.plug_in_pool;
 
 import android.os.Bundle;
-
+import java.time.LocalDateTime;
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 
-public class gestionMatch extends AppCompatActivity {
-
+public class gestionMatch extends AppCompatActivity
+{
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_gestion_match);
+    }
+    public class Match
+    {
+        String nom;
+        int nbManchesGagnantes;
+        LocalDateTime horodatage;
+        int gagnant;
+        private EtatMatch etat;
+    }
+    public enum EtatMatch
+    {
+        Cree(0),
+        EnCours(1),
+        Fini(2);
+        private final int etatMatch;
+        private Match match;
+        EtatMatch(int etatMatch)
+        {
+            this.etatMatch = etatMatch;
+        }
     }
 }
