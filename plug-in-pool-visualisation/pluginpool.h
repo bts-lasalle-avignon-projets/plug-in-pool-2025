f09d9cd201355aaebcef441abcee1ee6ea4a2112
Matteo Navarro <matteo.navarro.pro@gmail.com>
2025-03-26 15:07:48 +0100

    Ajouter association entre Match et PlugInPool

--- plug-in-pool-visualisation/pluginpool.h
+++ plug-in-pool-visualisation/pluginpool.h
@@ -4,12 +4,14 @@
 #include <QObject>
 
 class CommunicationBluetooth;
+class Match;
 
 class PlugInPool : public QObject
 {
     Q_OBJECT
   private:
     CommunicationBluetooth* communicationBluetooth;
+    Match*                  match;
 
   public:
     PlugInPool(QObject* parent = nullptr);
