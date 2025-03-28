e2a2f1dd5c0e29f1d8a29fbb73b9fd6c76e281ab
pmillotpro <pierre.millot.pro@gmail.com>
2025-03-28 11:06:46 +0100

    Ajouter evenement sur le bouton de navigation

--- plug-in-pool-mobile/app/src/main/java/com/example/plug_in_pool/ActiviteConfigurationMatch.java
+++ plug-in-pool-mobile/app/src/main/java/com/example/plug_in_pool/ActiviteConfigurationMatch.java
@@ -1,6 +1,10 @@
 package com.example.plug_in_pool;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -9,6 +13,8 @@ public class ActiviteConfigurationMatch extends AppCompatActivity
     /**
      * Constantes
      */
+    Button boutonLancerUnMatch;
+
     private static final String TAG = "_ActiviteConfigurationMatch"; //!< TAG pour les logs
 
     @Override
@@ -17,5 +23,20 @@ public class ActiviteConfigurationMatch extends AppCompatActivity
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_configuration_match);
+        boutonLancerUnMatch = findViewById(R.id.boutonLancerMatch);
+        jouerMatchActivite();
+    }
+
+    void jouerMatchActivite()
+    {
+        boutonLancerUnMatch.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                Intent changerDeVue = new Intent(ActiviteConfigurationMatch.this, ActiviteGestionMatch.class);
+                startActivity(changerDeVue);
+            }
+        });
     }
 }
