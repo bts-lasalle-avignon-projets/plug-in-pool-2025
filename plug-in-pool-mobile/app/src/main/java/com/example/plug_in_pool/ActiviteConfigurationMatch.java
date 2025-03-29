a3cf07d9e20419ad8eac0973659399f4a98d75df
pmillotpro <pierre.millot.pro@gmail.com>
2025-03-29 21:47:30 +0100

    Ajouter la création de nouveaux joueurs

--- plug-in-pool-mobile/app/src/main/java/com/example/plug_in_pool/ActiviteConfigurationMatch.java
+++ plug-in-pool-mobile/app/src/main/java/com/example/plug_in_pool/ActiviteConfigurationMatch.java
@@ -4,6 +4,8 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -14,6 +16,12 @@ public class ActiviteConfigurationMatch extends AppCompatActivity
      * Constantes
      */
     Button boutonLancerUnMatch;
+    Button creerJoueur1;
+    Button creerJoueur2;
+    EditText saisieNomJoueur1;
+    EditText saisiePrenomJoueur1;
+    EditText saisieNomJoueur2;
+    EditText saisiePrenomJoueur2;
 
     private static final String TAG = "_ActiviteConfigurationMatch"; //!< TAG pour les logs
 
@@ -23,10 +31,53 @@ public class ActiviteConfigurationMatch extends AppCompatActivity
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_configuration_match);
-        boutonLancerUnMatch = findViewById(R.id.boutonLancerMatch);
+        initialiserVues();
+        configurerMatch();
         jouerMatchActivite();
     }
-
+    void initialiserVues()
+    {
+        boutonLancerUnMatch = findViewById(R.id.boutonLancerMatch);
+        creerJoueur1 = findViewById(R.id.creerJoueur1);
+        creerJoueur2 = findViewById(R.id.creerJoueur2);
+        saisieNomJoueur1 = findViewById(R.id.saisieNomJoueur1);
+        saisiePrenomJoueur1 = findViewById(R.id.saisiePrenomJoueur1);
+        saisieNomJoueur2 = findViewById(R.id.saisieNomJoueur2);
+        saisiePrenomJoueur2 = findViewById(R.id.saisiePrenomJoueur2);
+    }
+    void configurerMatch()
+    {
+        creerDesJoueurs();
+    }
+    void creerDesJoueurs()
+    {
+        creerJoueur1.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                String nomJoueur1 = saisieNomJoueur1.getText().toString().trim();
+                String prenomJoueur1 = saisiePrenomJoueur1.getText().toString().trim();
+                Joueur joueur1 = new Joueur(nomJoueur1, prenomJoueur1);
+                joueur1.getJoueur();
+                joueur1.afficherJoueur();
+                Toast.makeText(ActiviteConfigurationMatch.this,"Votre nom : " + nomJoueur1 + "\n" + "Votre prénom : " + prenomJoueur1, Toast.LENGTH_SHORT).show();
+            }
+        });
+        creerJoueur2.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                String nomJoueur2 = saisieNomJoueur2.getText().toString().trim();
+                String prenomJoueur2 = saisiePrenomJoueur2.getText().toString().trim();
+                Joueur joueur2 = new Joueur(nomJoueur2, prenomJoueur2);
+                joueur2.getJoueur();
+                joueur2.afficherJoueur();
+                Toast.makeText(ActiviteConfigurationMatch.this,"Votre nom : " + nomJoueur2 + "\n" + "Votre prénom : " + prenomJoueur2, Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
     void jouerMatchActivite()
     {
         boutonLancerUnMatch.setOnClickListener(new View.OnClickListener()
--- plug-in-pool-mobile/app/src/main/java/com/example/plug_in_pool/Joueur.java
+++ plug-in-pool-mobile/app/src/main/java/com/example/plug_in_pool/Joueur.java
@@ -13,22 +13,40 @@ public class Joueur
      */
     private static final String TAG = "_Produit"; //!< TAG pour les logs
 
-    private String        nom;
-    private String        prenom;
-    private int           points;
-    private int           id;
-    private int           nbBillesRestantes = BlackBall.NB_BILLES_COULEUR;
-    private CouleurBille  couleur;
+    private String nom;
+    private String prenom;
+    private int points;
+    private int id;
+    private int nbBillesRestantes = BlackBall.NB_BILLES_COULEUR;
+    private CouleurBille couleur;
     private List<Empoche> empoches;
 
     public Joueur(String nom, String prenom, int points, int id)
     {
         Log.d(TAG,
-              "Produit() nom = " + nom + " - prenom = " + prenom + " - points = " + points +
-                " - id = " + id);
-        this.nom    = nom;
+                "Produit() nom = " + nom + " - prenom = " + prenom + " - points = " + points +
+                        " - id = " + id);
+        this.nom = nom;
         this.prenom = prenom;
         this.points = points;
-        this.id     = id;
+        this.id = id;
+    }
+
+    public Joueur(String nom, String prenom)
+    {
+        this.nom = nom;
+        this.prenom = prenom;
+        this.points = 0;
+        this.id = 0;
+    }
+
+    public String getJoueur()
+    {
+        return nom + prenom;
+    }
+
+    public void afficherJoueur()
+    {
+        Log.d("Joueur", "Nom : " + nom + " | Prénom : " + prenom);
     }
 }
